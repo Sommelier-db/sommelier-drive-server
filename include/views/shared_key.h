7627fb2637f21d68a0e5c88004be41d860d63483
hamadatakaki <i16hamadat@gmail.com>
2022-10-26 19:17:08 +0900

    update: replace sqlite3 object to SommelierDBMS object

--- include/views/shared_key.h
+++ include/views/shared_key.h
@@ -6,4 +6,4 @@
 json_t *get_api_shared_key_request(struct mg_str s);
 json_t *post_api_shared_key_request(struct mg_str s);
 void api_shared_key_view(struct mg_connection *, struct mg_http_message *,
-                         sqlite3 *);
+                         SommelierDBMS *);
